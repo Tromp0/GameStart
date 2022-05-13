5d2917f1efab0fdde93174c48e6780bd99c2fe4e
Tomás Lopez Meza <tomas.lopez@uvic.cat>
2022-05-13 19:02:44 +0200

    Fixed schemapath error

--- utilities/src/test/java/cat/uvic/teknos/m06/gamestart/utilities/XmlSchemaLoaderTest.java
+++ utilities/src/test/java/cat/uvic/teknos/m06/gamestart/utilities/XmlSchemaLoaderTest.java
@@ -9,7 +9,7 @@ public class XmlSchemaLoaderTest {
         var connectionProperties = new ConnectionProperties();
         connectionProperties.setUrl("jdbc:mysql://localhost:3306/");
         connectionProperties.setUsername("root");
-        var schemaLoader = new XmlSchemaLoader("C:\\Users\\10034356\\source\\repos\\gamestart_uf2\\utilities\\src\\test\\resources\\schema.xml", connectionProperties);
+        var schemaLoader = new XmlSchemaLoader("src\\test\\resources\\schema.xml", connectionProperties);
 
         assertDoesNotThrow(() -> {
             schemaLoader.load();
